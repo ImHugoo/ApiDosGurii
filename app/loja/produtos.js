372cd96bdf8d45cf8412336881912e52a0298937
Hugo Henrique <imhugooooo@gmail.com>
2024-06-07 21:10:26 -0300

    seila nem eu to entendendo mais

--- app/loja/produtos.js
+++
@@ -1,8 +0,0 @@
-export default function Produtos(){
-    return(
-            <div>
-                <p>consumir produtos da loja</p>
-                <button onClick={()=> alteraAutenticado(false)}>Desconectar</button>
-            </div>
-    );
-}
--- app/page.js
+++ app/page.js
@@ -1,28 +1,61 @@
-import Image from "next/image";
-import Cabecalho from "./components/cabecalho";
-import Conteudo from "./components/conteudo";
-import Ls_produtos from "./components/Lista_de_produtos";
-import Rodape from "./components/rodape";
+"use client"
 
-export default function Home() {
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./dog.css"
 
-  return (
-    <div>
+export default function Dogs(){
 
-      <Cabecalho/>
+    const [racas, alteraRacas] = useState([]);
+    const [imagem, alteraIMG] = useState([]);
+    const [nome, alteraNome] = useState([]);
 
-      <hr/>
+    function buscarDogguinho(){
 
-      <Conteudo/>
+        axios.get("https://dog.ceo/api/breeds/list/all")
+        .then( function(response){
+            alteraRacas( Object.keys( response.data.message ) )
+        })
+    }
 
-      <hr/>
+    function buscarIMG(raca_dog){
+        axios.get("https://dog.ceo/api/breed/"+raca_dog+"/images")
+        .then( function(response){
+            alteraIMG(( response.data.message ))
+        })
+    }
 
-      <Ls_produtos/>
+    function buscarTodos(){
+        axios.get("https://dog.ceo/api/breeds/image/random/20")
+        .then( function(response){
+            alteraIMG(( response.data.message ))
+        })
+    }
 
-      <hr/>
+    useEffect( () => {
+        buscarDogguinho();
+        buscarTodos();
+    } , []);
 
-      <Rodape/>
-
+    return(
+    <div id="dog">
+        <div>
+            <h1>Página dos dog</h1>
+            <h2> consulte e encontre fotos de dogguinhos</h2>
+        </div>
+        <div className="layout-central">
+            <div className="layout-menu">
+                <h3>dogguinhos</h3>
+                <ul>
+                    <li onClick={()=>{buscarTodos(); alteraNome("Todos os dog")}}>Todos os dog</li>
+                    {racas.map((r, index) => {return(<li key={r} onClick={()=> {buscarIMG(r); alteraNome(r)}}>{r}</li>)})}
+                </ul>
+            </div>
+            <div>
+                <h2>{nome}</h2>
+                {imagem.map((i, index) => {return(<img key={i} src={i}/>)})}
+            </div>
+        </div>
     </div>
-  );
+    );
 }
